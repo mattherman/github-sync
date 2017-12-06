44b3a18935ab92d69e5ab6a1a555c3d2fa247db8
Matthew Herman <mattherman11@gmail.com>
2017-12-05 22:20:00 -0600

    Added repo retrieval logic

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
     "flag"
     "os/exec"
     "os"
+    "encoding/json"
+    "net/http"
+    "io/ioutil"
 )
 
 var username = flag.String("username", "", "the user who owns the repositories to be synced")
@@ -15,8 +18,16 @@ func main() {
     fmt.Println("username=" + *username)
     fmt.Println("root=" + *rootDir)
 
-    repoUrl := "https://github.com/" + *username + "/MbDotNet.git"
-    cmdText := "git -C " + *rootDir + " clone " + repoUrl
+    var repositories = retrieveRepositories(*username)
+    if repositories == nil {
+        fmt.Printf("Unable to retrieve repositories for user %s, exiting...", username)
+    }
+
+    for _, repo := range repositories {
+        fmt.Println(repo.Name)
+    }
+    repoUrl := fmt.Sprintf("https://github.com/%s/MbDotNet.git", *username)
+    cmdText := fmt.Sprintf("\nexec: git -C %s clone %s", *rootDir, repoUrl)
     fmt.Println(cmdText)
     cmd := exec.Command("git", "-C", *rootDir, "clone", repoUrl)
     cmd.Stdout = os.Stdout
@@ -25,4 +36,41 @@ func main() {
     if err != nil {
         fmt.Println(err)
     }
+}
+
+func retrieveRepositories(username string) []Repo {
+    url := fmt.Sprintf("https://api.github.com/users/%s/repos", username)
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        fmt.Println("http.NewRequest: ", err)
+        return nil 
+    }
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println("http.Client.Do: ", err)
+        return nil
+    }
+
+    defer resp.Body.Close()
+
+    var repos []Repo
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println("ioutil.ReadAll: ", err)
+        return nil
+    }
+
+    if err := json.Unmarshal(body, &repos); err != nil {
+        fmt.Println(err)
+        return nil
+    }
+
+    return repos
+}
+
+type Repo struct {
+    Name string `json:"name"`
 }
